11a2fa1c074437035aeac5c399cf94ae69fbb396
mahieurobindev <94044404+mahieurobindev@users.noreply.github.com>
2022-03-15 16:25:34 +0100

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StudentComponent } from './student/student.component';
+import { StudentListComponent } from './student/pages/student-list/student-list.component';
+import { StudentDetailsComponent } from './student/pages/student-details/student-details.component';
+import { StudentFormComponent } from './student/components/student-form/student-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentComponent,
+    StudentListComponent,
+    StudentDetailsComponent,
+    StudentFormComponent
   ],
   imports: [
     BrowserModule,
