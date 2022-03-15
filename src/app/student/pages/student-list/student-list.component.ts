4f0af6b849d4b20a7e418b4044eaf08e8052b667
mahieurobindev <94044404+mahieurobindev@users.noreply.github.com>
2022-03-15 18:53:00 +0100

    add

--- src/app/student/pages/student-list/student-list.component.ts
+++ src/app/student/pages/student-list/student-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Students } from 'src/app/core/models/student';
 
 @Component({
   selector: 'app-student-list',
@@ -6,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./student-list.component.scss']
 })
 export class StudentListComponent implements OnInit {
-
-  constructor() { }
+  student$ : Observable<Students[]> | undefined;
+  private _StudentService: any;
+ 
+  constructor(private _studentService) { }
 
   ngOnInit(): void {
+    this.student$ = this._StudentService.get();
   }
 
 }
